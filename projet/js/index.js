a05a4576d9b9a851c80ad76e6bb9945d5e133e3e
Valérie <46691592+Val3rie2604@users.noreply.github.com>
2019-03-06 21:37:31 -0500

    Update index.js

--- projet/js/index.js
+++ projet/js/index.js
@@ -1,4 +1,4 @@
-/****************** Partie Amélie **********************/
+/****************** Menu de gauche **********************/
 
 function ouvreMG() {
 document.getElementById("menuGauche").style.width = "350px";
@@ -16,10 +16,10 @@ document.getElementById("entete").style.marginLeft = "0";
 $(document).ready(function() {
   $("#NMBouton").click(function() {
     $("#nouveauxMessages").show();
-	$("#nouveauxMessages").css("visibility", "visible");
-	$("#messagesEnvoyes").hide();
+  $("#nouveauxMessages").css("visibility", "visible");
+  $("#messagesEnvoyes").hide();
     $("#boiteReception").hide();
-    $("#contact").hide();
+    $("#contacts").hide();
   });
 });
 
@@ -28,7 +28,7 @@ $(document).ready(function() {
     $("#nouveauxMessages").hide();
     $("#messagesEnvoyes").hide();
     $("#boiteReception").show();
-    $("#contact").hide();
+    $("#contacts").hide();
   });
 });
 
@@ -36,9 +36,9 @@ $(document).ready(function() {
   $("#MEBouton").click(function() {
     $("#nouveauxMessages").hide();
     $("#messagesEnvoyes").show();
-	$("#messagesEnvoyes").css("visibility", "visible");
+  $("#messagesEnvoyes").css("visibility", "visible");
     $("#boiteReception").hide();
-    $("#contact").hide();
+    $("#contacts").hide();
   });
 });
 $(document).ready(function() {
@@ -46,54 +46,52 @@ $(document).ready(function() {
     $("#nouveauxMessages").hide();
     $("#messagesEnvoyes").hide();
     $("#boiteReception").hide();
-    $("#contact").show();
-	$("#contact").css("visibility", "visible");
+    $("#contacts").show();
+  $("#contacts").css("visibility", "visible");
   });
 });
 
  
-
  
- 
-/**************************** Partie Sarah *****************************/
+/**************************** Boite de réception*****************************/
 function afficherAlbert(){
-	document.getElementById("Nom").innerHTML="De:    Albert J.  ";
-	document.getElementById("Sujet").innerHTML="Sujet: Retour de mon voyage ";
-	document.getElementById("paragraphe").innerHTML=" Salut, je voulais te dire que je suis revenue de mon voyage. C'était un si beau voyage! On pourrait se voir pour que je te montre quelques photos. ";
-	document.getElementById("signature").innerHTML=" -Albert ";
+  document.getElementById("Nom").innerHTML="De:    Albert J.  ";
+  document.getElementById("Sujet").innerHTML="Sujet: Retour de mon voyage ";
+  document.getElementById("paragraphe").innerHTML=" Salut, je voulais te dire que je suis revenue de mon voyage. C'était un si beau voyage! On pourrait se voir pour que je te montre quelques photos. ";
+  document.getElementById("signature").innerHTML=" -Albert ";
 
 }
 
 function afficherGinette(){
-	document.getElementById("Nom").innerHTML="De: Ginette A. ";
-	document.getElementById("Sujet").innerHTML="Sujet: De tes nouvelles. ";
-	document.getElementById("paragraphe").innerHTML=" Allo, je voulais voir de tes nouvelles. J'esp�re que tu vas bien. Appelle moi svp. ";
-	document.getElementById("signature").innerHTML=" -Ginette ";
+  document.getElementById("Nom").innerHTML="De: Ginette A. ";
+  document.getElementById("Sujet").innerHTML="Sujet: De tes nouvelles. ";
+  document.getElementById("paragraphe").innerHTML=" Allo, je voulais voir de tes nouvelles. J'espère que tu vas bien. Appelle moi svp. ";
+  document.getElementById("signature").innerHTML=" -Ginette ";
 }
 
 function afficherBob(){
-	document.getElementById("Nom").innerHTML="De: Bob E. ";
-	document.getElementById("Sujet").innerHTML="Sujet: J'ai ton livre ";
-	document.getElementById("paragraphe").innerHTML=" Hey, j'ai le livre dont tu as besoin pour ton cours d'informatique. Il est arriv� hier. Je te l'am�ne en cours demain. Bonne soir�e! ";
-	document.getElementById("signature").innerHTML=" -Bob ";
+  document.getElementById("Nom").innerHTML="De: Bob E. ";
+  document.getElementById("Sujet").innerHTML="Sujet: J'ai ton livre ";
+  document.getElementById("paragraphe").innerHTML=" Hey, j'ai le livre dont tu as besoin pour ton cours d'informatique. Il est arrivé hier. Je te l'amène en cours demain. Bonne soirée! ";
+  document.getElementById("signature").innerHTML=" -Bob ";
 }
 
 function afficherBen(){
-	document.getElementById("Nom").innerHTML="De: Ben M. ";
-	document.getElementById("Sujet").innerHTML="Sujet: Note de cours ";
-	document.getElementById("paragraphe").innerHTML=" Hey, est-ce que tu pourrais m'envoyer les notes de cours de programmation? j'ai du m'absenter du cours hier... Merci !!  ";
-	document.getElementById("signature").innerHTML=" -Ben ";
+  document.getElementById("Nom").innerHTML="De: Ben M. ";
+  document.getElementById("Sujet").innerHTML="Sujet: Note de cours ";
+  document.getElementById("paragraphe").innerHTML=" Hey, est-ce que tu pourrais m'envoyer les notes de cours de programmation? j'ai du m'absenter du cours hier... Merci !!  ";
+  document.getElementById("signature").innerHTML=" -Ben ";
 }
 
 function afficherNora(){
-	document.getElementById("Nom").innerHTML="Nora S.";
-	document.getElementById("Sujet").innerHTML="Party ";
-	document.getElementById("paragraphe").innerHTML=" Salut, oublies pas le party de vendredi soir !! si tu as des questions appelle moi. ";
-	document.getElementById("signature").innerHTML=" -Nora ";
+  document.getElementById("Nom").innerHTML="De: Nora S.";
+  document.getElementById("Sujet").innerHTML="Sujet: Party ";
+  document.getElementById("paragraphe").innerHTML=" Salut, oublies pas le party de vendredi soir !! si tu as des questions appelle moi. ";
+  document.getElementById("signature").innerHTML=" -Nora ";
 }
 
+/**************************** Contacts*****************************/
 
-/**************************** Partie Valérie *****************************/
 var fenetre = document.getElementById('fenetreContact');
 var btnAjout = document.getElementById("ajoutContact");
 var x = document.getElementsByClassName("fermer")[0];
